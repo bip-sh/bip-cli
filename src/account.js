5486a914c9d492a8fa552b94139060d3e3f0a89f
Joel Kennedy <joelkennedy@sky.com>
2020-08-16 15:20:00 +0100

    Fix command is not a function bug

--- src/account.js
+++ src/account.js
@@ -6,70 +6,42 @@ const emoji = require('node-emoji')
 const open = require('open');
 const prompts = require('prompts')
 
-module.exports = {
-  balanceCommand: async function () {
-    validation.requireApiKey();
+module.exports.balanceCommand = async function () {
+  validation.requireApiKey();
 
-    progress.spinner().start('Getting account balance');
-    let headers = {
-      'X-Api-Key': config.userpref.get('apiKey')
-    }
-    let init = {
-      headers: headers,
-      method: 'GET'
-    }
-    let response = await validation.safelyFetch(config.api.baseurl + 'account/balance', init)
-    let responseJson = await validation.safelyParseJson(response)
+  progress.spinner().start('Getting account balance');
+  let headers = {
+    'X-Api-Key': config.userpref.get('apiKey')
+  }
+  let init = {
+    headers: headers,
+    method: 'GET'
+  }
+  let response = await validation.safelyFetch(config.api.baseurl + 'account/balance', init)
+  let responseJson = await validation.safelyParseJson(response)
 
-    progress.spinner().stop();
+  progress.spinner().stop();
 
-    switch(response.status) {
-      case 200:
-        console.log(emoji.get('pound') + ' Your account balance is: ' + responseJson.symbol + responseJson.balance);
-        break;
-      default:
-        errors.returnServerError(response.status, responseJson);
-    }
-  },
-  setupPaymentCommand: async function () {
-    validation.requireApiKey();
+  switch(response.status) {
+    case 200:
+      console.log(emoji.get('pound') + ' Your account balance is: ' + responseJson.symbol + responseJson.balance);
+      break;
+    default:
+      errors.returnServerError(response.status, responseJson);
+  }
+}
+module.exports.setupPaymentCommand = async function () {
+  validation.requireApiKey();
 
-    const promptRes = await prompts({
-      type: 'confirm',
-      name: 'value',
-      message: 'Press Y to open a new browser window and attach a payment card to your account',
-      initial: true
-    });
+  const promptRes = await prompts({
+    type: 'confirm',
+    name: 'value',
+    message: 'Press Y to open a new browser window and attach a payment card to your account',
+    initial: true
+  });
 
-    // Continue if user confirms
-    if (promptRes.value) {
-      progress.spinner().start('Requesting data');
-      let headers = {
-        'X-Api-Key': config.userpref.get('apiKey'),
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-      let init = {
-        headers: headers,
-        method: 'GET'
-      }
-      let response = await validation.safelyFetch(config.api.baseurl + 'account/setuppayment', init)
-      let responseJson = await validation.safelyParseJson(response)
-
-      progress.spinner().stop();
-
-      switch(response.status) {
-        case 200:
-          await open(responseJson.url);
-          console.log(emoji.get('link') + ' Please follow the instructions in the newly opened browser window to attach a payment card to your account.');
-          break;
-        default:
-          errors.returnServerError(response.status, responseJson);
-      }
-    }
-  },
-  billingCommand: async function () {
-    validation.requireApiKey();
-
+  // Continue if user confirms
+  if (promptRes.value) {
     progress.spinner().start('Requesting data');
     let headers = {
       'X-Api-Key': config.userpref.get('apiKey'),
@@ -79,7 +51,7 @@ module.exports = {
       headers: headers,
       method: 'GET'
     }
-    let response = await validation.safelyFetch(config.api.baseurl + 'account/billingportal', init)
+    let response = await validation.safelyFetch(config.api.baseurl + 'account/setuppayment', init)
     let responseJson = await validation.safelyParseJson(response)
 
     progress.spinner().stop();
@@ -87,10 +59,36 @@ module.exports = {
     switch(response.status) {
       case 200:
         await open(responseJson.url);
-        console.log(emoji.get('link') + ' The billing portal has opened in a new window.');
+        console.log(emoji.get('link') + ' Please follow the instructions in the newly opened browser window to attach a payment card to your account.');
         break;
       default:
         errors.returnServerError(response.status, responseJson);
     }
-  },
-};
+  }
+}
+module.exports.billingCommand = async function () {
+  validation.requireApiKey();
+
+  progress.spinner().start('Requesting data');
+  let headers = {
+    'X-Api-Key': config.userpref.get('apiKey'),
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+  let init = {
+    headers: headers,
+    method: 'GET'
+  }
+  let response = await validation.safelyFetch(config.api.baseurl + 'account/billingportal', init)
+  let responseJson = await validation.safelyParseJson(response)
+
+  progress.spinner().stop();
+
+  switch(response.status) {
+    case 200:
+      await open(responseJson.url);
+      console.log(emoji.get('link') + ' The billing portal has opened in a new window.');
+      break;
+    default:
+      errors.returnServerError(response.status, responseJson);
+  }
+}
